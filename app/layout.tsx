01bacc96f7c08f91bce4364a69afdbc540834281
Blue-Onion <rawatadityasingh963@gmail.com>
2025-11-02 23:20:49 +0530

    Refactor layout.tsx: replace Geist font with Archivo Black and Inter, update className for improved styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Archivo_Black,Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({ subsets: ["latin"] });
+const archivoBlack = Archivo_Black({
   subsets: ["latin"],
+  weight: "400",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Artmeister",
-  description: "Art club",
+  description: "Art club-",
 };
 
 export default function RootLayout({
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${archivoBlack} antialiased`}
       >
         {children}
       </body>
