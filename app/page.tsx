afeb66a5ece961708c73172e3db90fd813fb515a
Blue-Onion <rawatadityasingh963@gmail.com>
2025-11-23 19:27:24 +0530

    feat: Add Hero component, new logo and video assets, remove default SVGs, and configure TypeScript for JSX.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-
-
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+import Hero from "@/components/landing/Hero";
 export default function Home() {
   return (
     <main className="bg-white">
-      
+      <Hero />
     </main>
   );
 }
---
+++ components/landing/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+    <>
+    <section id='hero' className='noisy'>
+
+        </section>
+    </>
+    )
+}
+
+export default Hero
